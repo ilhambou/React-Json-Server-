d5122eed6d3e4882274a2eec21d40a634354795d
ilhambou <82772375+ilhambou@users.noreply.github.com>
2023-11-23 02:49:55 +0100

    recherche

--- src/components/Products.js
+++ src/components/Products.js
@@ -3,8 +3,11 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import axios from 'axios';
 import React, { useEffect, useState } from 'react'
 import { checkProduct, deleteProduct, getProducts } from '../app/app';
+import { faSearch } from '@fortawesome/free-solid-svg-icons';
+
 
 function Products() {
+  const [query, setQuery] = useState("");
   const[state,setState]=useState( // object dans state 
     {
       products:[],
@@ -16,7 +19,7 @@ function Products() {
   )
 
   useEffect(()=>{
-    handleGetProducts(state.keyword, state.currentPage, state.pageSize);
+    handleGetProducts(state.keyword, state.currentPage, state.pageSize); // tous est reinitialise
   },[]);
 
 
@@ -55,13 +58,36 @@ function Products() {
   const handleGotoPage=(page)=>{
     handleGetProducts(state.keyword, page, state.pageSize);
   }
+  const handleSearch=(event)=>{
+    event.preventDefault();
+    handleGetProducts(query,1,state.pageSize)
+
+  }
 
   return (
     <div className='p-1 m-1'>
       <div className='row'>
         <div className='col-md-6'>
-         <div className='card'>
+          <div className='card m-1'>
           <div className='card-body'>
+            <form onSubmit={handleSearch}>
+              <div className='row g-2'>
+                <div className='col-auto'>
+                  <input value={query} onChange={(e)=>setQuery(e.target.value)} className='form-control'></input>
+                </div>
+                <div className='col-auto'>
+                 <button className='btn btn-success'>
+                  <FontAwesomeIcon icon={faSearch}></FontAwesomeIcon>
+                  </button>
+                </div>
+
+              </div>
+            </form>
+          </div>
+          </div>
+         <div className='card m-1'>
+          <div className='card-body'>
+            
             <table className='table'>
               <thead>
                 <tr>
@@ -101,8 +127,13 @@ function Products() {
                 {
                   (new Array(state.totalPages).fill(0)).map((v,index)=>( // emerna tbleau b les 0 ms ce qu il nous intesse howa index 
                     <li>
-                      <button onClick={()=>handleGotoPage(index+1)} className='btn btn-outline-info ms-1'>
-                        {index + 1}
+                      <button onClick={()=>handleGotoPage(index+1)}
+                      className={
+                        index + 1 == state.currentPage 
+                        ?"btn btn-info ms-1"
+                        :"btn btn-outline-info ms-1"}
+                      >
+                        {index + 1} 
                       </button>
                     </li>
                   ))
